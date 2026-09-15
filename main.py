bcd8e1edd65b4a4abe3c03eed2d78f4f9007b9a3
Sudeeneess <pleskovawaleria@gmail.com>
2026-09-15 11:16:43 +0700

    add settings

---
+++ main.py
@@ -0,0 +1,5 @@
+# import psycopg
+# import psycopg2.extras
+from settings import *
+
+print(db_host)
---
+++ settings.py
@@ -0,0 +1,5 @@
+db_host = '217.71.129.139'
+db_port = 6346
+db_user = 'Sudeeneess'
+db_pass = 'zytgjvygfhjkm'
+db_name = 'Sudo'
